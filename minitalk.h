e3d3dc19204e4722f101cbc8824b6419e32aca50
Paulo Marques <paulormarques21@gmail.com>
2021-10-02 17:16:49 -0300

    Header config

---
+++ minitalk.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: proberto <proberto@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/24 15:39:06 by proberto          #+#    #+#             */
+/*   Updated: 2021/10/02 14:57:12 by proberto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <sys/types.h>
+# include <unistd.h>
+# include <signal.h>
+# include <bits/sigaction.h>
+# include <limits.h>
+# include <stdlib.h>
+
+# define BYTE 8
+
+#endif
